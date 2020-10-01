43774001b3ba8fd4d7b12268d3d7a39b08d824d6
Shamil Sultanov <shamil.amirovich@gmail.com>
2020-10-01 23:28:51 +0300

    Update build.js, package.json

--- build.js
+++ build.js
@@ -3,6 +3,8 @@ const { writeFileSync, readFileSync, copyFileSync } = require('fs');
 const packageJson = JSON.parse(readFileSync('package.json').toString());
 
 delete packageJson.scripts;
+delete packageJson.jest;
+delete packageJson.nodemonConfig;
 
 writeFileSync('dist/package.json', JSON.stringify(packageJson, null, 2));
 
